b363000d18802d0c74786a68a560e427c9196964
Samuel Gratzl <samuel_gratzl@gmx.at>
2016-11-24 16:03:22 +0100

    update boilerplate code

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+const margin = {
+    top: 40,
+    bottom: 10,
+    left: 20,
+    right: 20
+};
+const width = 800 - margin.left - margin.right;
+const height = 600 - margin.top - margin.bottom;
 
-d3.select('body').append('div').html('Hello World');
+// Creates sources <svg> element and inner g (for margins)
+const svg = d3.select('body').append('svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+    .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+/////////////////////////
